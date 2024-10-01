ad684db2258821d6a3123a525d44bab94c1a22bb
Aleksandr Zakharchuk <allexss86@gmail.com>
2024-10-01 23:01:32 +0300

    Решенная задача 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,22 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Кот " + bean3.getName() + ", " + "возраст " + bean3.getAge() + " года");
+
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Кот " + bean4.getName() + ", " + "возраст " + bean4.getAge() + " года");
+
+        System.out.println(bean == bean2);
+        if (bean3 == bean4) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
